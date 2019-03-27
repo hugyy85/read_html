f3e3ac4ffccc5751dca598dc0bc72a0522942267
hugyy85 <46883700+hugyy85@users.noreply.github.com>
2019-03-27 14:44:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import read_html, config
 
 def _main(url):
-    site = read_html.Find_text(url, config.file_name_setting)
+    site = read_html.Findtext(url, config.file_name_setting)
     text = site.find_text()
     site.write_text_to_file(text)
     file_name = site.new_dir + site.new_name
-    format_text = read_html.Formating_file(file_name)
+    format_text = read_html.Formatingfile(file_name)
     format_text.width_of_string()
 
 if __name__ == '__main__':
